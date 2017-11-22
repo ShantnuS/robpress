cc556b60e164a0892444634dcb8b5567bf19db27
Phil <philipcumner@gmail.com>
2017-11-22 17:10:18 +0000

    Add solution for XSS
    *  Multiple parts:
    	* Used $f3->clean($input) to remove tags for each input
    	* Add form helper to hopefully aid the use of the aforementioned
    	* function
    	* Whitespace changes

--- helpers/authhelper.php
+++ helpers/authhelper.php
@@ -9,9 +9,9 @@
 
 		/** Attempt to resume a previously logged in session if one exists */
 		public function resume() {
-			$f3=Base::instance();				
+			$f3=Base::instance();
 
-			//Ignore if already running session	
+			//Ignore if already running session
 			if($f3->exists('SESSION.user.id')) return;
 
 			//Log user back in from cookie
@@ -19,7 +19,7 @@
 				$user = unserialize(base64_decode($f3->get('COOKIE.RobPress_User')));
 				$this->forceLogin($user);
 			}
-		}		
+		}
 
 		/** Perform any checks before starting login */
 		public function checkLogin($username,$password,$request,$debug) {
@@ -27,25 +27,25 @@
 			//DO NOT check login when in debug mode
 			if($debug == 1) { return true; }
 
-			return true;	
+			return true;
 		}
 
 		/** Look up user by username and password and log them in */
 		public function login($username,$password) {
-			$f3=Base::instance();						
+			$f3=Base::instance();
 			$db = $this->controller->db;
 			$results = $db->query("SELECT * FROM `users` WHERE `username`='$username' AND `password`='$password'");
-			if (!empty($results)) {		
-				$user = $results[0];	
+			if (!empty($results)) {
+				$user = $results[0];
 				$this->setupSession($user);
 				return $this->forceLogin($user);
-			} 
+			}
 			return false;
 		}
 
 		/** Log user out of system */
 		public function logout() {
-			$f3=Base::instance();							
+			$f3=Base::instance();
 
 			//Kill the session
 			session_destroy();
@@ -106,13 +106,13 @@
 			}
 
 			//Log in as new user
-			return $this->forceLogin($user);			
+			return $this->forceLogin($user);
 		}
 
 		/** Force a user to log in and set up their details */
 		public function forceLogin($user) {
 			//YOU ARE NOT ALLOWED TO CHANGE THIS FUNCTION
-			$f3=Base::instance();					
+			$f3=Base::instance();
 
 			if(is_object($user)) { $user = $user->cast(); }
 
